9019aff61467b67daaf66a2afb21cd43ce36d09b
JosephHo7 <118572699+JosephHo7@users.noreply.github.com>
2023-05-20 13:30:18 -0400

    create loginview

--- src/Components/login-view/login-view.jsx
+++ src/Components/login-view/login-view.jsx
@@ -14,17 +14,26 @@ export const LoginView = ({onLoggedIn}) => {
 
         fetch("https://movieapi-lcrt.onrender.com/login", {
             method: "POST",
-            headers: {"Content-Type": "application.json"},
+            headers: {"Content-Type": "application/json"},
             body: JSON.stringify(data)
-        }).then((response) => 
+        })
+        .then ((response) => response.json())
+        .then((data) => 
             {
-            if (response.ok) {
-                onLoggedIn(username);
+            console.log("Login Response:", data)
+            if (data.user) {
+                localStorage.setItem("user", JSON.stringify(data.user));
+                localStorage.setItem("token", data.token);
+                onLoggedIn(data.user, data.token);
               } else {
-                alert("Login Failed");
+                alert("No Such User");
               }
             })
-          };
+            .catch((e) =>{
+                alert("Something went wrong");
+            });
+            
+    }
       
          
     
